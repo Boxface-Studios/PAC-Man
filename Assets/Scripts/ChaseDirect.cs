ad5d30cc28e4740ff472b9a36a2e2ca13f50f083
ClockworkProduction <51160854+ClockworkProduction@users.noreply.github.com>
2020-07-16 21:28:41 +0800

    added new chase script

---
+++ Assets/Scripts/ChaseDirect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ChaseDirect : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {        
+        //chases the player directly
+        agent.destination = points[destPoint].position;
+    }
+}
