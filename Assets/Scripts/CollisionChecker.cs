afb33b640ad0bc914e2fd90cb5d86cd4c75c2f0d
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-07-22 12:20:22 +0800

    Wed 1

---
+++ Assets/Scripts/CollisionChecker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour
+{
+
+    public int damageDeltToCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("collision");
+        Debug.Log(collision.collider.gameObject);
+
+        if (collision.collider.gameObject.GetComponent<Health>())
+        {
+           
+            collision.collider.gameObject.GetComponent<Health>().TakeDamage(damageDeltToCollision);
+        }
+        
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,35 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public GameObject playerThree;
+    public GameObject playerFour;
+
+    public Health HealthP1;
+    public Health HealthP2;
+    public Health HealthP3;
+    public Health HealthP4;
+
+    public Image HBar1;
+    public Image HBar2;
+    public Image HBar3;
+    public Image HBar4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthP1 = playerOne.GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HBar1.fillAmount = HealthP1.health / 100;
+    }
+}
