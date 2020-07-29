742e779071388c3092d993fef28799cba0e873bf
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-07-29 11:23:04 +0800

    wed 3

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
+    //player is 1. 2 for 2 players 4 for multi 
     public GameObject playerOne;
     public GameObject playerTwo;
     public GameObject playerThree;
     public GameObject playerFour;
+    //for zombies
+    public GameObject playerFive;
+    public GameObject playerSix;
 
     public Health HealthP1;
     public Health HealthP2;
     public Health HealthP3;
     public Health HealthP4;
+    //for zombies
+    public Health HealthP5;
+    public Health HealthP6;
 
     public Image HBar1;
     public Image HBar2;
