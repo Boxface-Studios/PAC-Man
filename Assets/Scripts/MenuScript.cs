8eca97ef9e0f2bf3c276ba529bd913c99795e3ad
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-07-29 19:48:46 +0800

    Menu update

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void SinglePlayer()
+    {
+        //SceneManager.LoadScene("LevelDesign", LoadSceneMode.Single);
+
+    }
+
+    public void DuoPlayer()
+    {
+        //SceneManager.LoadScene("EvansBonusLevel", LoadSceneMode.Single);
+
+    }
+
+    public void TrioPlayer()
+    {
+       // SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void QuadPlayer()
+    {
+        //SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Ded");
+        Application.Quit();
+    }
+}
