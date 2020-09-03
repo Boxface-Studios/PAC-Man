b5a4953ef5ce2a236d4db6722868f4c4fe38cad1
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-09-04 03:56:15 +0800

    new push

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (this.tag == "PACMAN")
+        {
+            if (other.tag == "Pickup")
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
